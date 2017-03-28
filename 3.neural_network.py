2fd7656b026c7219a2644cdfa066898ff72715ff
David <davidyap93@gmail.com>
2017-03-29 04:10:28 +0800

    Initial

--- 3.neural_network.py
+++ 3.neural_network.py
@@ -3,53 +3,34 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 
-"""
-A typical training procedure for a neural network is as follows:
 
-- Define the neural network that has some learnable parameters (or weights)
-- Iterate over a dataset of inputs
-- Process input through the network
-    the loss (how far is the output from being correct)
-- Propagate gradients back into the network’s parameters
-- Update the weights of the network, typically using a simple update rule:
-    weight = weight + learning_rate * gradient
-"""
-
-
-# Define network
 class Net(nn.Module):
+
     def __init__(self):
         super(Net, self).__init__()
+        # 1 input image channel, 6 output channels, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # an affine operation: y = Wx + b
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))  # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)  # If the size is a square you can only specify a single number
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
     def num_flat_features(self, x):
-        size = x.size()[1:]
+        size = x.size()[1:]  # all dimensions except the batch dimension
         num_features = 1
         for s in size:
-            num_features *= 5
+            num_features *= s
         return num_features
 
 
 net = Net()
 print(net)
-
-
-params = list(net.parameters())
-print(len(params))
-print(params[0].size()) # conv1's weight
-
-input = Variable(torch.randn(1, 1, 32, 32))
-out = net(input)
-print(out)
